e509c761fd8ab004b4fe019b5e77378aac19881c
jenn kaplan <jenn.kaplan@plangrid.com>
2018-05-31 11:43:42 -0700

    removed flashing rainbow effect for rainbow wave effect

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -297,10 +297,6 @@ KALEIDOSCOPE_INIT_PLUGINS(
     // We start with the LED effect that turns off all the LEDs.
     LEDOff,
 
-    // The rainbow effect changes the color of all of the keyboard's keys at the same time
-    // running through all the colors of the rainbow.
-    LEDRainbowEffect,
-
     // The rainbow wave effect lights up your keyboard with all the colors of a rainbow
     // and slowly moves the rainbow across your keyboard
     LEDRainbowWaveEffect,
